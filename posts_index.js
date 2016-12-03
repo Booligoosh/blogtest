4cdde9c2cb8179db5b509feb1d6feca6ea2dd359
Booligoosh <booligoosh@optusnet.com.au>
2016-12-03 15:21:19 +1100

    updated framework

--- posts_index.js
+++ posts_index.js
@@ -1,6 +1,9 @@
-var posts_folder = 'posts';
+var posts_folder = '/posts';
 var posts = ['test.html'];
 for (var i = 0; i < posts.length; i++) {
   posts[i] = posts_folder + '/' + posts[i];
 }
-console.log(posts);
+for (var i = 0; i < posts.length; i++) {
+    var name = '[name]'
+    document.getElementsByClassName('posts')[0].innerHTML = document.getElementsByClassName('posts')[0].innerHTML + '<a href="' + posts[i] + '" class="sidebar_link sidebar_posts_link"><li>' + name + '</li></a>'
+}
