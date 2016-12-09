9accc534e4c8bdc23b68a289d6a83f4f9ccb941a
Booligoosh <booligoosh@optusnet.com.au>
2016-12-09 20:33:59 +1100

    tried timeout

--- end_script.js
+++ end_script.js
@@ -1,7 +1,7 @@
 document.getElementsByClassName('post')[0].innerHTML = document.getElementsByClassName('post_data')[0].innerHTML;
 document.title = document.getElementById('data_title').innerHTML;
 
-$(document).ready(function(){
+/*$(document).ready(function(){
     for (var i = 0; i < posts.length; i++) {
         var post_html = posts_html[i];
         var link = posts[i];
@@ -11,4 +11,16 @@ $(document).ready(function(){
         console.log(name + ' - ' + posts + ' - ' + i + ' - ' + posts[i] + ' - ' + link);
 
     }
-});
+});*/
+
+$(document).ready(function(){  setTimeout(function(){
+for (var i = 0; i < posts.length; i++) {
+    var post_html = posts_html[i];
+    var link = posts[i];
+    console.log(i);
+    name = $(post_html)[5].childNodes[1].innerHTML;
+    document.getElementsByClassName('posts')[0].innerHTML = document.getElementsByClassName('posts')[0].innerHTML + '<a href="' + link + '" class="sidebar_link sidebar_posts_link"><li>' + name + '</li></a>'
+    console.log(name + ' - ' + posts + ' - ' + i + ' - ' + posts[i] + ' - ' + link);
+
+}
+},5000);});
