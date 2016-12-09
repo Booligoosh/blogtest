5e71c6fba8a297c412b82c0a9d1606b20fd062e3
Booligoosh <booligoosh@optusnet.com.au>
2016-12-09 18:14:27 +1100

    Made sidebar names work

--- posts_index.js
+++ posts_index.js
@@ -1,9 +1,15 @@
 var posts_folder = '/blog/posts';
-var posts = ['test.html','test2.html'];
+var posts = ['test.html', 'test2.html'];
 for (var i = 0; i < posts.length; i++) {
-  posts[i] = posts_folder + '/' + posts[i];
+    posts[i] = posts_folder + '/' + posts[i];
 }
+
 for (var i = 0; i < posts.length; i++) {
-    var name = '[name]'
+    var sidebar_link_post_html;
+    $.get(posts[i], function (data) {
+        sidebar_link_post_html = data;
+    });
+    console.log(sidebar_link_post_html);
+    var name = sidebar_link_post_html.match(/<title[^>]*>([^<]+)<\/title>/)[1];
     document.getElementsByClassName('posts')[0].innerHTML = document.getElementsByClassName('posts')[0].innerHTML + '<a href="' + posts[i] + '" class="sidebar_link sidebar_posts_link"><li>' + name + '</li></a>'
 }
